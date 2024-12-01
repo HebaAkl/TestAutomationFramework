a3cc0a6ad4451e3895d4ffb5caa210add34c6d0d
HebaAkl <hebahmed2016@gmail.com>
2024-12-01 11:21:35 +0200

    [Add] models for serialize/deserilaize api json

---
+++ src/main/java/models/Product.java
@@ -0,0 +1,25 @@
+package models;
+
+public class Product {
+    private String title;
+    private int id;
+
+    public Product() {
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+}
---
+++ src/main/java/models/User.java
@@ -0,0 +1,13 @@
+package models;
+
+public class User {
+    private String firstName;
+
+    public User() {
+
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+}
